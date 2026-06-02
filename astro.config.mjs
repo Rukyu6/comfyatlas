2a4b30d5f586716859f664f4215880cdeb4312a0
Rukyu6 <rukyu6@users.noreply.github.com>
2026-06-02 19:45:10 +0800

    chore: revert domain back to comfyatlas.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.puppyshop.com',
+  site: 'https://comfyatlas.com',
   integrations: [
     mdx(),
     sitemap(),
