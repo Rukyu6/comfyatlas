e829ea0e50049545d7849c45b3a92eb533f8ffe9
Rukyu6 <rukyu6@users.noreply.github.com>
2026-06-02 19:43:01 +0800

    chore: add www subdomain to config urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://puppyshop.com',
+  site: 'https://www.puppyshop.com',
   integrations: [
     mdx(),
     sitemap(),
