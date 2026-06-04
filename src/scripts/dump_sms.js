ef12194a03e406c5ed71d16e6ac1b8d5d4bcbd09
Rukyu6 <rukyu6@users.noreply.github.com>
2026-06-04 12:36:48 +0800

    chore: remove temporary verification scripts

--- src/scripts/dump_sms.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-const data = JSON.parse(fs.readFileSync('src/data/tutorials.json', 'utf8'));
-const item = data.find(x => x.id === '019c078f-95f5-71c5-a8f9-3176f61a80e5');
-if (item) {
-  console.log(item.content_html);
-}
--- src/scripts/test_ids.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const data = JSON.parse(fs.readFileSync('src/data/tutorials.json', 'utf8'));
-data.forEach(x => {
-  console.log(`${x.id} | ${x.name} | ${x.parent_name}`);
-});
